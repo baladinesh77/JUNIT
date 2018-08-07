de8f2026640f28378d5a09cd0b2d6666a189bd3e
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2018-08-07 15:10:19 +0530

    Implementation for methods

--- TestDemo2/src/com/syntel/ProductDAO.java
+++ TestDemo2/src/com/syntel/ProductDAO.java
@@ -1,5 +1,6 @@
 package com.syntel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDAO {
@@ -11,21 +12,40 @@ public class ProductDAO {
 	}
 	
 	public void save(Product p) {
-		
+		products.add(p);
 	}
 	
 	public Product findProduct(String name) {
-		
+		for(Product p: products) {
+			if(p.getName().equalsIgnoreCase(name.trim()))
+				return p;
+		}
+		return null;
 	}
 	
 	public List<Product> findProductBelowPrice(float price){
-		
+		List<Product> temp = new ArrayList<>();
+		for(Product p : products) {
+			if(p.getPrice()<price) {
+				temp.add(p);
+			}
+		}
+		return temp;
 	}
 	public List<Product> findProductAbovePrice(float price){
-		
+		List<Product> temp = new ArrayList<>();
+		for(Product p : products) {
+			if(p.getPrice()>price) {
+				temp.add(p);
+			}
+		}
+		return temp;
 	}
 	
 	public void updatePrice(String name,float price) {
-		
+		Product p = findProduct(name);
+		if(p!=null) {
+			p.setPrice(price);
+		}
 	}
 }
