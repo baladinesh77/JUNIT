14f8405ee11a571a806ffe1ebfceb0e2a7bde2e2
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2018-08-07 15:15:22 +0530

    Test classes added
    Test classes with comment (No code)

---
+++ TestDemo2/srctest/com/syntel/ProductDAOTest1.java
@@ -0,0 +1,44 @@
+package com.syntel;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test cases for Product that exists!
+ * @author mahendra
+ *
+ */
+public class ProductDAOTest1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testFindProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindProductBelowPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindProductAbovePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePrice() {
+		fail("Not yet implemented");
+	}
+
+}
